765d068e945291546cfc974ba82155a3cb2a55e3
ahusnain <ahussnain.ka@emaar.ae>
2019-03-17 16:48:13 +0400

    Flutter Layout Basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './screens/Home.dart';
+import './screens/layout.dart';
 
 /// [main] Entry point of a Flutter App
 /// [runApp] inflate the widget and attached it to screen
@@ -16,12 +16,7 @@ class HelloWorldApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //disapear debug tag
       title: "Hello Flutter App",
-      home: Scaffold(
-        appBar: AppBar(
-            title: Text("Hello Flutter App")
-        ),
-        body: Home(),
-      ),
+      home: Layout(),
     );
   }
 
